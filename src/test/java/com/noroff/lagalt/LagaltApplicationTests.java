c07d0eb4a988216b82bcfdae8e79d5a7e13d62e6
Bjørnar Pedersen <bjornar.pedersen@no.experis.com>
2021-03-10 15:51:04 +0100

    initial commit

---
+++ src/test/java/com/noroff/lagalt/LagaltApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noroff.lagalt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LagaltApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
