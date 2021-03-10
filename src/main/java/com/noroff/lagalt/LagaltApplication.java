59b0c3eb15c2dc549983095d2d9ad3c01e7a2d9f
Bjørnar Pedersen <bjornar.pedersen@no.experis.com>
2021-03-10 15:51:59 +0100

    hello world

--- src/main/java/com/noroff/lagalt/LagaltApplication.java
+++ src/main/java/com/noroff/lagalt/LagaltApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LagaltApplication {
 
 	public static void main(String[] args) {
-
+		//Hei fra bjørnar
 		SpringApplication.run(LagaltApplication.class, args);
 	}
 
